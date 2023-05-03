37bfcbc1c699817b96a351ea9f33424381435497
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-05-03 14:59:26 -0700

    change json file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.use(cors());
 
@@ -11,6 +12,13 @@ app.get('/', (req, res) =>{
     res.send('Recipe and chefs is running')
 
 });
+app.get('/recipes', (req, res) =>{
+    // console.log(category);
+    res.send(recipes);
+
+})
+
+
 
 app.get('/chefs',(req, res) =>{
     res.send(chefs);
@@ -21,6 +29,13 @@ app.get('/chefs/:id',(req, res) =>{
     // console.log(id);
     const selectedChefs = chefs.find(c=> c._id === id);
     res.send(selectedChefs)
+})
+app.get('/recipes/:id',(req,res) =>{
+    const id = req.params.id;
+    // console.log(id);
+    const chefsRecipes = recipes.find(r=> r._id === id)
+    res.send(chefsRecipes);
+
 })
 
 app.listen(port, () =>{
